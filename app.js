0340c15460a2b0ee34d8f61b912b7b61a6f61638
Kevin Salina <kevin.salina25@gmail.com>
2021-06-16 18:28:11 -0400

    Reworked scss file structure. Added about.html page with styles

--- app.js
+++ app.js
@@ -1,41 +1,3 @@
-// Mobile Navigation
-const navSlide = () => {
-    const burger = document.querySelector('.hamburger')
-    const nav = document.querySelector('.nav-item-group-mobile')
-    const navItems = nav.querySelectorAll('.nav-item')
-    const htmlTag = document.querySelector('html')
-    const bodyTag = document.querySelector('body')
-
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('active')
-        nav.classList.toggle('active')
-        htmlTag.classList.toggle('active')
-        bodyTag.classList.toggle('active')
-        slideShowDisplay.classList.toggle('active')
-        navItems.forEach((item, index) => {
-            item.classList.toggle('active')
-            if (item.style.animation) {
-                item.style.animation = '';
-            } else {
-                item.style.animation = `navItemFade .5s ease forwards ${index / 7 + .5}s`
-            }
-        })
-    })
-
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 900) {
-            nav.classList.remove('active')
-            htmlTag.classList.remove('active')
-            bodyTag.classList.remove('active')
-            slideShowDisplay.classList.remove('active')
-            navItems.forEach(item => {
-                item.classList.remove('active')
-                item.style.animation = '';
-            })
-        }
-    })
-}
-
 // Slideshow
 let i = 0;
 let slideShowImages = [];
@@ -88,14 +50,12 @@ slideShowButtons.forEach(btn => {
 // Helper SlideShow CSS Function
 function slideShowCSS() {
     if (window.innerWidth > 450) {
-        console.log('greater than 450')
         slideShowDisplay.style.cssText = `
         background: url(${slideShowImages[i]}) center;
         background-size: cover;
         background-attachment: fixed;
         `
     } else {
-        console.log('less than 450')
         slideShowDisplay.style.cssText = `
         background: url(${slideShowImages[i]}) center;
         background-size: cover;
@@ -105,7 +65,24 @@ function slideShowCSS() {
 }
 
 
+// Nav
+const homeNavSlide = () => {
+    const burger = document.querySelector('.hamburger')
+
+    burger.addEventListener('click', () => {
+        slideShowDisplay.classList.toggle('active')
+    })
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 900) {
+            slideShowDisplay.classList.remove('active')
+        }
+    })
+}
+
+
 // Call Functions
-navSlide()
+homeNavSlide()
 window.onload = changeSlideShow(true)
+
 
---
+++ scripts/navbar.js
@@ -0,0 +1,37 @@
+// Mobile Navigation
+const navSlide = () => {
+    const burger = document.querySelector('.hamburger')
+    const nav = document.querySelector('.nav-item-group-mobile')
+    const navItems = nav.querySelectorAll('.nav-item')
+    const htmlTag = document.querySelector('html')
+    const bodyTag = document.querySelector('body')
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active')
+        nav.classList.toggle('active')
+        htmlTag.classList.toggle('active')
+        bodyTag.classList.toggle('active')
+        navItems.forEach((item, index) => {
+            item.classList.toggle('active')
+            if (item.style.animation) {
+                item.style.animation = '';
+            } else {
+                item.style.animation = `navItemFade .5s ease forwards ${index / 7 + .5}s`
+            }
+        })
+    })
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 900) {
+            nav.classList.remove('active')
+            htmlTag.classList.remove('active')
+            bodyTag.classList.remove('active')
+            navItems.forEach(item => {
+                item.classList.remove('active')
+                item.style.animation = '';
+            })
+        }
+    })
+}
+
+navSlide()
